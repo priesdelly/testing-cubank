19e7428540bcf23482cd328b5e80463b79d52270
Saridraj <saridrajsapsin@gmail.com>
2022-11-21 16:24:46 +0700

    update withdraw test script

--- cypress/e2e/spec_withdraw.cy.ts
+++ cypress/e2e/spec_withdraw.cy.ts
@@ -1,10 +1,38 @@
-describe('empty spec', () => {
+describe('withdraw TC1', () => {
     it('passes', () => {
       cy.visit('https://cu-bank-fe.vercel.app/')
     })
   
-    it('click', () => {
-      cy.get('button').click();
+    it('withdraw', () => {
+      cy.get('#accountId')
+      .type('4444444444')
+      .get('#password')
+      .type('4444')
+      .get('[cid="lc"]')
+      .click()
+      .get('[cid="w1"]')
+      .type('0')
+      .get('[cid="wc"]')
+      .click()
+      
     })
-  
   })
+
+  // describe('withdraw TC2', () => {
+  //   it('passes', () => {
+  //     cy.visit('https://cu-bank-fe.vercel.app/')
+  //   })
+  
+  //   it('withdraw', () => {
+  //     cy.get('#accountId')
+  //     .type('4444444444')
+  //     .get('#password')
+  //     .type('4444')
+  //     .get('[cid="lc"]')
+  //     .click()
+  //     .get('[cid="w1"]')
+  //     .type('10')
+  //     .get('[cid="wc"]')
+  //     .click()
+  //   })
+  // })
