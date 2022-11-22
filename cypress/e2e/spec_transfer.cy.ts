78fa794df083aa8486df2a8bc274498c98e9f0b6
Waris Punnahitanond <waris46842@hotmail.com>
2022-11-22 22:13:13 +0700

    add more testcases

--- cypress/e2e/spec_transfer.cy.ts
+++ cypress/e2e/spec_transfer.cy.ts
@@ -131,7 +131,7 @@ context('Transfer system', () => {
       transfer(account_id1, old_balance_id2+100)
     })
 
-    it("alert balance not enough", ()=> {
+    it("Alert your balance isn't not enough", ()=> {
       cy.get(':nth-child(3) > label').should("have.text", "your balance isn't not enough");
     })
 
@@ -162,8 +162,6 @@ context('Transfer system', () => {
   });
 
   describe("TC2", () => {
-    
-    transfer_amount = 100
 
     it("visit site", () => {
       visit_site()
@@ -202,6 +200,7 @@ context('Transfer system', () => {
     })
 
     it("transfer when balance is enough", ()=> {
+      transfer_amount = old_balance_id2
       transfer(account_id1, transfer_amount)
     })
 
@@ -232,8 +231,6 @@ context('Transfer system', () => {
   });
 
   describe("TC3", () => {
-    
-    transfer_amount = 99
 
     it("visit site", () => {
       visit_site()
@@ -272,6 +269,7 @@ context('Transfer system', () => {
     })
 
     it("transfer when balance is enough", ()=> {
+      transfer_amount = old_balance_id2
       transfer(account_id1, transfer_amount)
     })
 
@@ -539,7 +537,7 @@ context('Transfer system', () => {
       transfer(account_id1, transfer_amount)
     })
 
-    it("alert Please put only number", ()=> {
+    it("Alert please put only number", ()=> {
       cy.get(':nth-child(3) > label').should("have.text", "Please put only number");
     })
 
@@ -635,6 +633,739 @@ context('Transfer system', () => {
 
     it("Check old_balance_id1 = new_balance_id1", ()=> {
       expect(old_balance_id2 == new_balance_id2 + parseFloat(transfer_amount)).to.be.true
+    })
+
+  });
+
+  describe("TC9", () => {
+    
+    transfer_amount = 1.01
+
+    it("visit site", () => {
+      visit_site()
+    })
+
+    it("login and get old balance id1", () => {
+      login_and_get_balance(account_id1, password_1, false)
+    })
+
+    it("logout", ()=> {
+      logout()
+    })
+
+    it("visit site", () => {
+      visit_site()
+    })
+
+    it("login and get old balance id2", () => {
+      login_and_get_balance(account_id2, password_2, false)
+    })
+
+    it("transfer 1.01", ()=> {
+      cy.get("#accountId").clear();
+      cy.get("#accountId").type(account_id1);
+      cy.get(
+        ":nth-child(5) > :nth-child(2) > form > :nth-child(2) > label > #among"
+      ).clear();
+      cy.get(
+        ":nth-child(5) > :nth-child(2) > form > :nth-child(2) > label > #among"
+      ).type(String(1.01));
+      cy.get(":nth-child(5) > :nth-child(2) > form > button").click();
+      cy.get(":nth-child(5) > :nth-child(2) > form > :nth-child(2) > label > #among")
+      .then(($el) => $el[0].checkValidity())
+      .should("be.false");
+    })
+
+    it("logout", ()=> {
+      logout()
+    })
+
+    it("login and get new balance id1", () => {
+      login_and_get_balance(account_id1, password_1, true)
+    })
+
+    it("Check old_balance_id1 = new_balance_id1", ()=> {
+      expect(old_balance_id1 == new_balance_id1).to.be.true
+    })
+
+    it("logout", ()=> {
+      logout()
+    })
+
+    it("login and get new balance id2", () => {
+      login_and_get_balance(account_id2, password_2, true)
+    })
+
+    it("Check old_balance_id1 = new_balance_id1", ()=> {
+      expect(old_balance_id2 == new_balance_id2).to.be.true
+    })
+
+  });
+
+  describe("TC10", () => {
+    
+    transfer_amount = 1.0000000597
+
+    it("visit site", () => {
+      visit_site()
+    })
+
+    it("login and get old balance id1", () => {
+      login_and_get_balance(account_id1, password_1, false)
+    })
+
+    it("logout", ()=> {
+      logout()
+    })
+
+    it("visit site", () => {
+      visit_site()
+    })
+
+    it("login and get old balance id2", () => {
+      login_and_get_balance(account_id2, password_2, false)
+    })
+
+    it("transfer 1.01", ()=> {
+      cy.get("#accountId").clear();
+      cy.get("#accountId").type(account_id1);
+      cy.get(
+        ":nth-child(5) > :nth-child(2) > form > :nth-child(2) > label > #among"
+      ).clear();
+      cy.get(
+        ":nth-child(5) > :nth-child(2) > form > :nth-child(2) > label > #among"
+      ).type(String(1.01));
+      cy.get(":nth-child(5) > :nth-child(2) > form > button").click();
+      cy.get(":nth-child(5) > :nth-child(2) > form > :nth-child(2) > label > #among")
+      .then(($el) => $el[0].checkValidity())
+      .should("be.false");
+    })
+
+    it("logout", ()=> {
+      logout()
+    })
+
+    it("login and get new balance id1", () => {
+      login_and_get_balance(account_id1, password_1, true)
+    })
+
+    it("Check old_balance_id1 = new_balance_id1", ()=> {
+      expect(old_balance_id1 == new_balance_id1).to.be.true
+    })
+
+    it("logout", ()=> {
+      logout()
+    })
+
+    it("login and get new balance id2", () => {
+      login_and_get_balance(account_id2, password_2, true)
+    })
+
+    it("Check old_balance_id1 = new_balance_id1", ()=> {
+      expect(old_balance_id2 == new_balance_id2).to.be.true
+    })
+
+  });
+
+  describe("TC11", () => {
+    
+    transfer_amount = '1.0000000596'
+
+    it("visit site", () => {
+      visit_site()
+    })
+
+    it("login and get old balance id1", () => {
+      login_and_get_balance(account_id1, password_1, false)
+    })
+
+    it("logout", ()=> {
+      logout()
+    })
+
+    it("visit site", () => {
+      visit_site()
+    })
+
+    it("login and get old balance id2", () => {
+      login_and_get_balance(account_id2, password_2, false)
+    })
+
+    it("deposit", () => {
+      deposit(deposit_amount)
+    })
+
+    it("logout", ()=> {
+      logout()
+    })
+
+    it("visit site", () => {
+      visit_site()
+    })
+
+    it("login and get old balance id2", () => {
+      login_and_get_balance(account_id2, password_2, false)
+    })
+
+    it("transfer when balance is enough", ()=> {
+      transfer(account_id1, transfer_amount)
+    })
+
+    it("logout", ()=> {
+      logout()
+    })
+
+    it("login and get new balance id1", () => {
+      login_and_get_balance(account_id1, password_1, true)
+    })
+
+    it("Check old_balance_id1 = new_balance_id1", ()=> {
+      expect(old_balance_id1 + Math.round(parseFloat(transfer_amount)) == new_balance_id1).to.be.true
+    })
+
+    it("logout", ()=> {
+      logout()
+    })
+
+    it("login and get new balance id2", () => {
+      login_and_get_balance(account_id2, password_2, true)
+    })
+
+    it("Check old_balance_id2 = new_balance_id2", ()=> {
+      expect(old_balance_id2 == new_balance_id2 + Math.round(parseFloat(transfer_amount))).to.be.true
+    })
+
+  });
+
+  describe("TC13", () => {
+    
+    transfer_amount = -1
+
+    it("visit site", () => {
+      visit_site()
+    })
+
+    it("login and get old balance id1", () => {
+      login_and_get_balance(account_id1, password_1, false)
+    })
+
+    it("logout", ()=> {
+      logout()
+    })
+
+    it("visit site", () => {
+      visit_site()
+    })
+
+    it("login and get old balance id2", () => {
+      login_and_get_balance(account_id2, password_2, false)
+    })
+
+    it("transfer -1", ()=> {
+      transfer(account_id1, transfer_amount)
+    })
+
+    it("Alert please put only number", ()=> {
+      cy.get(':nth-child(3) > label').should("have.text", "Please put only number");
+    })
+
+    it("logout", ()=> {
+      logout()
+    })
+
+    it("login and get new balance id1", () => {
+      login_and_get_balance(account_id1, password_1, true)
+    })
+
+    it("Check old_balance_id1 = new_balance_id1", ()=> {
+      expect(old_balance_id1 == new_balance_id1).to.be.true
+    })
+
+    it("logout", ()=> {
+      logout()
+    })
+
+    it("login and get new balance id2", () => {
+      login_and_get_balance(account_id2, password_2, true)
+    })
+
+    it("Check old_balance_id1 = new_balance_id1", ()=> {
+      expect(old_balance_id2 == new_balance_id2).to.be.true
+    })
+
+  });
+
+  describe("TC17", () => {
+    
+    transfer_amount = '1e999'
+
+    it("visit site", () => {
+      visit_site()
+    })
+
+    it("login and get old balance id1", () => {
+      login_and_get_balance(account_id1, password_1, false)
+    })
+
+    it("logout", ()=> {
+      logout()
+    })
+
+    it("visit site", () => {
+      visit_site()
+    })
+
+    it("login and get old balance id2", () => {
+      login_and_get_balance(account_id2, password_2, false)
+    })
+
+    it("transfer 1e999", ()=> {
+      cy.get("#accountId").clear();
+      cy.get("#accountId").type(account_id1);
+      cy.get(
+        ":nth-child(5) > :nth-child(2) > form > :nth-child(2) > label > #among"
+      ).clear();
+      cy.get(
+        ":nth-child(5) > :nth-child(2) > form > :nth-child(2) > label > #among"
+      ).type(transfer_amount);
+      cy.get(":nth-child(5) > :nth-child(2) > form > button").click();
+      cy.get(":nth-child(5) > :nth-child(2) > form > :nth-child(2) > label > #among")
+      .then(($el) => $el[0].checkValidity())
+      .should("be.false");
+    })
+
+    it("logout", ()=> {
+      logout()
+    })
+
+    it("login and get new balance id1", () => {
+      login_and_get_balance(account_id1, password_1, true)
+    })
+
+    it("Check old_balance_id1 = new_balance_id1", ()=> {
+      expect(old_balance_id1 == new_balance_id1).to.be.true
+    })
+
+    it("logout", ()=> {
+      logout()
+    })
+
+    it("login and get new balance id2", () => {
+      login_and_get_balance(account_id2, password_2, true)
+    })
+
+    it("Check old_balance_id1 = new_balance_id1", ()=> {
+      expect(old_balance_id2 == new_balance_id2).to.be.true
+    })
+
+  });
+
+describe("TC18", () => {
+    
+    transfer_amount = '1e99'
+
+    it("visit site", () => {
+      visit_site()
+    })
+
+    it("login and get old balance id1", () => {
+      login_and_get_balance(account_id1, password_1, false)
+    })
+
+    it("logout", ()=> {
+      logout()
+    })
+
+    it("visit site", () => {
+      visit_site()
+    })
+
+    it("login and get old balance id2", () => {
+      login_and_get_balance(account_id2, password_2, false)
+    })
+
+    it("transfer when balance is enough", ()=> {
+      transfer(account_id1, transfer_amount)
+    })
+
+    it("logout", ()=> {
+      logout()
+    })
+
+    it("login and get new balance id1", () => {
+      login_and_get_balance(account_id1, password_1, true)
+    })
+
+    it("Check old_balance_id1 = new_balance_id1", ()=> {
+      expect(old_balance_id1 + Number(transfer_amount[0]) == new_balance_id1).to.be.true
+    })
+
+    it("logout", ()=> {
+      logout()
+    })
+
+    it("login and get new balance id2", () => {
+      login_and_get_balance(account_id2, password_2, true)
+    })
+
+    it("Check old_balance_id2 = new_balance_id2", ()=> {
+      expect(old_balance_id2 == new_balance_id2 + Number(transfer_amount[0])).to.be.true
+    })
+
+  });
+
+  describe("TC19", () => {
+    
+    transfer_amount = '1e9'
+
+    it("visit site", () => {
+      visit_site()
+    })
+
+    it("login and get old balance id1", () => {
+      login_and_get_balance(account_id1, password_1, false)
+    })
+
+    it("logout", ()=> {
+      logout()
+    })
+
+    it("visit site", () => {
+      visit_site()
+    })
+
+    it("login and get old balance id2", () => {
+      login_and_get_balance(account_id2, password_2, false)
+    })
+
+    it("transfer when balance is enough", ()=> {
+      transfer(account_id1, transfer_amount)
+    })
+
+    it("logout", ()=> {
+      logout()
+    })
+
+    it("login and get new balance id1", () => {
+      login_and_get_balance(account_id1, password_1, true)
+    })
+
+    it("Check old_balance_id1 = new_balance_id1", ()=> {
+      expect(old_balance_id1 + Number(transfer_amount[0]) == new_balance_id1).to.be.true
+    })
+
+    it("logout", ()=> {
+      logout()
+    })
+
+    it("login and get new balance id2", () => {
+      login_and_get_balance(account_id2, password_2, true)
+    })
+
+    it("Check old_balance_id2 = new_balance_id2", ()=> {
+      expect(old_balance_id2 == new_balance_id2 + Number(transfer_amount[0])).to.be.true
+    })
+
+  });
+
+  describe("TC20", () => {
+    
+    transfer_amount = 50
+
+    it("visit site", () => {
+      visit_site()
+    })
+
+    it("login and get old balance id1", () => {
+      login_and_get_balance(account_id1, password_1, false)
+    })
+
+    it("logout", ()=> {
+      logout()
+    })
+
+    it("visit site", () => {
+      visit_site()
+    })
+
+    it("login and get old balance id2", () => {
+      login_and_get_balance(account_id2, password_2, false)
+    })
+
+    it("deposit", () => {
+      deposit(deposit_amount)
+    })
+
+    it("logout", ()=> {
+      logout()
+    })
+
+    it("visit site", () => {
+      visit_site()
+    })
+
+    it("login and get old balance id2", () => {
+      login_and_get_balance(account_id2, password_2, false)
+    })
+
+    it("transfer when account_id is incorrect", ()=> {
+      transfer(account_id1.concat('1'), transfer_amount)
+    })
+
+    it("Alert please fill accountId 10 digits", ()=> {
+      cy.get(':nth-child(3) > label').should("have.text", "Please fill accountId  10 digits");
+    })
+
+    it("logout", ()=> {
+      logout()
+    })
+
+    it("login and get new balance id1", () => {
+      login_and_get_balance(account_id1, password_1, true)
+    })
+
+    it("Check old_balance_id1 = new_balance_id1", ()=> {
+      expect(old_balance_id1 == new_balance_id1).to.be.true
+    })
+
+    it("logout", ()=> {
+      logout()
+    })
+
+    it("login and get new balance id2", () => {
+      login_and_get_balance(account_id2, password_2, true)
+    })
+
+    it("Check old_balance_id2 = new_balance_id2", ()=> {
+      expect(old_balance_id2 == new_balance_id2).to.be.true
+    })
+
+  });
+
+  describe("TC21", () => {
+    
+    transfer_amount = 50
+
+    it("visit site", () => {
+      visit_site()
+    })
+
+    it("login and get old balance id1", () => {
+      login_and_get_balance(account_id1, password_1, false)
+    })
+
+    it("logout", ()=> {
+      logout()
+    })
+
+    it("visit site", () => {
+      visit_site()
+    })
+
+    it("login and get old balance id2", () => {
+      login_and_get_balance(account_id2, password_2, false)
+    })
+
+    it("deposit", () => {
+      deposit(deposit_amount)
+    })
+
+    it("logout", ()=> {
+      logout()
+    })
+
+    it("visit site", () => {
+      visit_site()
+    })
+
+    it("login and get old balance id2", () => {
+      login_and_get_balance(account_id2, password_2, false)
+    })
+
+    it("transfer when account_id is incorrect", ()=> {
+      transfer(account_id1.slice(0,9), transfer_amount)
+    })
+
+    it("Alert please fill accountId 10 digits", ()=> {
+      cy.get(':nth-child(3) > label').should("have.text", "Please fill accountId  10 digits");
+    })
+
+    it("logout", ()=> {
+      logout()
+    })
+
+    it("login and get new balance id1", () => {
+      login_and_get_balance(account_id1, password_1, true)
+    })
+
+    it("Check old_balance_id1 = new_balance_id1", ()=> {
+      expect(old_balance_id1 == new_balance_id1).to.be.true
+    })
+
+    it("logout", ()=> {
+      logout()
+    })
+
+    it("login and get new balance id2", () => {
+      login_and_get_balance(account_id2, password_2, true)
+    })
+
+    it("Check old_balance_id2 = new_balance_id2", ()=> {
+      expect(old_balance_id2 == new_balance_id2).to.be.true
+    })
+
+  });
+
+  describe("TC22", () => {
+    
+    transfer_amount = 50
+
+    it("visit site", () => {
+      visit_site()
+    })
+
+    it("visit site", () => {
+      visit_site()
+    })
+
+    it("login and get old balance id2", () => {
+      login_and_get_balance(account_id2, password_2, false)
+    })
+
+    it("deposit", () => {
+      deposit(deposit_amount)
+    })
+
+    it("logout", ()=> {
+      logout()
+    })
+
+    it("visit site", () => {
+      visit_site()
+    })
+
+    it("login and get old balance id2", () => {
+      login_and_get_balance(account_id2, password_2, false)
+    })
+
+    it("transfer when account_id is incorrect", ()=> {
+      transfer('abcdef1234', transfer_amount)
+    })
+
+    it("Alert please put accountId only number", ()=> {
+      cy.get(':nth-child(3) > label').should("have.text", "Please put accountId only number");
+    })
+
+    it("logout", ()=> {
+      logout()
+    })
+
+    it("login and get new balance id1", () => {
+      login_and_get_balance(account_id2, password_2, true)
+    })
+
+    it("Check old_balance_id2 = new_balance_id2", ()=> {
+      expect(old_balance_id2 == new_balance_id2).to.be.true
+    })
+
+  });
+
+  describe("TC23", () => {
+    
+    transfer_amount = 50
+
+    it("visit site", () => {
+      visit_site()
+    })
+
+    it("login and get old balance id2", () => {
+      login_and_get_balance(account_id2, password_2, false)
+    })
+
+    it("deposit", () => {
+      deposit(deposit_amount)
+    })
+
+    it("logout", ()=> {
+      logout()
+    })
+
+    it("visit site", () => {
+      visit_site()
+    })
+
+    it("login and get old balance id2", () => {
+      login_and_get_balance(account_id2, password_2, false)
+    })
+
+    it("transfer when account_id is incorrect", ()=> {
+      transfer('1312312121', transfer_amount)
+    })
+
+    it("Alert not found your target account Id", ()=> {
+      cy.get(':nth-child(3) > label').should("have.text", "Not found your target account Id");
+    })
+
+    it("logout", ()=> {
+      logout()
+    })
+
+    it("login and get new balance id2", () => {
+      login_and_get_balance(account_id2, password_2, true)
+    })
+
+    it("Check old_balance_id2 = new_balance_id2", ()=> {
+      expect(old_balance_id2 == new_balance_id2).to.be.true
+    })
+
+  });
+
+  describe("TC24", () => {
+    
+    transfer_amount = 50
+
+    it("visit site", () => {
+      visit_site()
+    })
+
+    it("login and get old balance id2", () => {
+      login_and_get_balance(account_id2, password_2, false)
+    })
+
+    it("deposit", () => {
+      deposit(deposit_amount)
+    })
+
+    it("logout", ()=> {
+      logout()
+    })
+
+    it("visit site", () => {
+      visit_site()
+    })
+
+    it("login and get old balance id2", () => {
+      login_and_get_balance(account_id2, password_2, false)
+    })
+
+    it("transfer when account_id is incorrect", ()=> {
+      transfer(account_id2, transfer_amount)
+    })
+
+    it("Alert cannot transfer to your own id", ()=> {
+      cy.get(':nth-child(3) > label').should("have.text", "Cannot transfer to your own id");
+    })
+
+    it("logout", ()=> {
+      logout()
+    })
+
+    it("login and get new balance id2", () => {
+      login_and_get_balance(account_id2, password_2, true)
+    })
+
+    it("Check old_balance_id2 = new_balance_id2", ()=> {
+      expect(old_balance_id2 == new_balance_id2).to.be.true
     })
 
   });
